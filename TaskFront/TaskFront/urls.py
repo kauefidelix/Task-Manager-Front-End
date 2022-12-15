6ec5fa9e22eb407bc8cbd232b6979659b754aab8
kauefidelix <47992593+kauefidelix@users.noreply.github.com>
2022-12-15 02:38:49 -0300

    Create app and test urls

--- TaskFront/TaskFront/urls.py
+++ TaskFront/TaskFront/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from core import views
 
 urlpatterns = [
+    path(r'^$', views.index),
+    path(r'first_app/',include('core.urls')),
     path('admin/', admin.site.urls),
 ]
