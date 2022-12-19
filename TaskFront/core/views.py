a4a0df5de490a9d7a6467787b9dc712847c9a5e7
kauefidelix <47992593+kauefidelix@users.noreply.github.com>
2022-12-19 09:48:21 -0300

    tests

--- TaskFront/core/views.py
+++ TaskFront/core/views.py
@@ -4,4 +4,5 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the TaskFront index.")
+    my_dict = {'insert_me': "Hello I am from views.py!"}
+    return render(request, 'index.html', context=my_dict)
