fb78a6dd09b9120983e0654a1d57ac2689164d26
kauefidelix <47992593+kauefidelix@users.noreply.github.com>
2022-12-24 15:13:45 -0300

    Remove test codes

--- TaskFront/core/views.py
+++ TaskFront/core/views.py
@@ -4,5 +4,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    my_dict = {'insert_me': "Hello I am from views.py!"}
-    return render(request, 'index.html', context=my_dict)
+    return render(request, 'index.html')
