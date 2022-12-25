11851e74cbebb22f4c447fae662ac0f1a437c9e5
kauefidelix <47992593+kauefidelix@users.noreply.github.com>
2022-12-25 03:45:05 -0300

    Make rtp column dynamic

--- TaskFront/core/views.py
+++ TaskFront/core/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from core.models import Card
+from datetime import date
 # Create your views here.
 
 def index(request):
-    return render(request, 'index.html')
+    cards = Card.objects.all()
+    today = date.today()
+    return render(request, 'index.html', {'cards': cards, 'today': today})
