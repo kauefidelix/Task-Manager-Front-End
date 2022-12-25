bf0cf668648d69f0206e18abb3484846bfd485c2
kauefidelix <47992593+kauefidelix@users.noreply.github.com>
2022-12-25 03:44:09 -0300

    Create models for cards and epics

--- TaskFront/core/admin.py
+++ TaskFront/core/admin.py
@@ -1,7 +1,5 @@
 from django.contrib import admin
-from core.models import Topic, Webpage, AccessRecord
+from core.models import Epic, Card
 # Register your models here.
-admin.site.register(Topic)
-admin.site.register(Webpage)
-admin.site.register(AccessRecord)
-
+admin.site.register(Epic)
+admin.site.register(Card)
--- TaskFront/core/models.py
+++ TaskFront/core/models.py
@@ -1,24 +1,28 @@
 from django.db import models
+import uuid
 
 # Create your models here.
 
-class Topic(models.Model):
-    top_name = models.CharField(max_length=264, unique=True)
-
-    def __str__(self):
-        return self.top_name
-
-class Webpage(models.Model):
-    topic = models.ForeignKey(Topic,on_delete=models.CASCADE)
+class Epic(models.Model):
+    epic_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=264, unique=True)
-    url = models.URLField(unique=True)
-
+    content = models.TextField()
+    creation_date = models.DateField()
+    last_modified = models.DateField()
+    assignee = models.CharField(max_length=264, unique=True)
     def __str__(self):
-        return self.name
+        return str(self.name)
 
-class AccessRecord(models.Model):
-    name = models.ForeignKey(Webpage,on_delete=models.CASCADE)
-    date = models.DateField()
 
+class Card(models.Model):
+    card_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=264, unique=True)
+    content = models.TextField()
+    creation_date = models.DateField()
+    last_modified = models.DateField()
+    column = models.CharField(max_length=264, unique=True)
+    assignee = models.CharField(max_length=264, unique=True)
+    card_type = models.CharField(max_length=264, unique=True)
+    epic_link = models.ForeignKey(Epic, on_delete=models.CASCADE)
     def __str__(self):
-        return str(self.date)
+        return str(self.card_id)
