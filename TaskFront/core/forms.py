3f924c4abfb1e280fc51c7f79ff7929d026f93f1
kaue.fidelix <kaue.fidelix@ifood.com.br>
2023-01-01 15:16:28 -0300

    add card view test

---
+++ TaskFront/core/forms.py
@@ -0,0 +1,7 @@
+from django import forms
+from .models import Card
+
+class CardForm(forms.ModelForm):
+    class Meta:
+        model = Card
+        fields = ['name', 'content', 'creation_date', 'last_modified', 'column', 'assignee', 'card_type', 'epic_link']
--- TaskFront/core/views.py
+++ TaskFront/core/views.py
@@ -8,3 +8,18 @@ def index(request):
     cards = Card.objects.all()
     today = date.today()
     return render(request, 'index.html', {'cards': cards, 'today': today})
+
+def view_card(request, card_id):
+    card = get_object_or_404(Card, pk=card_id)
+    if request.method == 'POST':
+        form = CardForm(request.POST, instance=card)
+        if form.is_valid():
+            form.save()
+            return redirect('view_card', card_id=card.card_id)
+    else:
+        form = CardForm(instance=card)
+    context = {
+        'card': card,
+        'form': form,
+    }
+    return render(request, 'card.html', context)
