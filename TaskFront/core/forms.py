419b698b0a4a084de7897ad7d593f685e7479feb
kauefidelix <47992593+kauefidelix@users.noreply.github.com>
2023-01-03 22:52:26 -0300

    fix card detail view

--- TaskFront/core/forms.py
+++ TaskFront/core/forms.py
@@ -1,7 +1,7 @@
-from django import forms
-from .models import Card
+#from django import forms
+#from .models import Card
 
-class CardForm(forms.ModelForm):
-    class Meta:
-        model = Card
-        fields = ['name', 'content', 'creation_date', 'last_modified', 'column', 'assignee', 'card_type', 'epic_link']
+#class CardForm(forms.ModelForm):
+#    class Meta:
+#        model = Card
+#        fields = ['name', 'content', 'creation_date', 'last_modified', 'column', 'assignee', 'card_type', 'epic_link']
